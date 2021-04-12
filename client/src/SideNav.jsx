6ea32d216fb52e2effdb73f8420e791df987ee3e
A.C_Codes <62225871+ACabrera-Codes@users.noreply.github.com>
2021-04-11 22:53:52 -0400

    Create a post skeleton

---
+++ client/src/SideNav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SideNav = (props) => {
+
+return (
+
+<div className= "sidenav">
+   <a href="#section">About</a>
+   <a href="#section">Home</a>
+   <a href="#section">Submissions</a>
+   <a href="#section">Contact</a>
+</div>
+
+ );
+};
+
+export default SideNav;
